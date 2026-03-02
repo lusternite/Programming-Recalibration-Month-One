85e51cc61197931aee7257057b4ba634e03f7b59
lusternite <luster_nite@hotmail.com>
2026-03-02 23:38:20 +0900

    Init
    Tonight I struggled to do hello world and make a function. We are learning.

---
+++ Recalibration Week 1/Recalibration Week 1/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Simon is coding...");
+
+        Console.WriteLine(add(1, 2));
+    }
+
+    private static int add(int a, int b)
+    {
+        return a + b;
+    }
+}
